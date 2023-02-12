988d59f18c22f0926bc9d8f376e08d7e3a3cf1df
Kirti Singh <kirtisingh1497@gmail.com>
2023-02-12 23:08:44 +0530

    feat: working on tip per person calculation

--- script.js
+++ script.js
@@ -5,26 +5,34 @@ const fifteenPer =  document.querySelector('#fifteen-per')
 const twentyPer =  document.querySelector('#twenty-five-per')
 const  fiftyPer =  document.querySelector('#fifty-per')
 const  custom =  document.querySelector('#custom')
+const totalPeople = document.querySelector('#noOfPeople')
 
-
+let tipAmountCalculated;
 function tipPercentage(perc){
     perc = perc.value;
     console.log(perc);
-    let tipAmountCalculated;
     let billAmount = inputBill.value;
     if(billAmount == ''){
         console.log('Please enter input bill')
     }else{
-        tipAmountCalculated = calculateTipAmount(perc,billAmount)
+        tipAmountCalculated = calculatetotalTipAmount(perc,billAmount)
         console.log(tipAmountCalculated)
     }
 }
 
-function calculateTipAmount(amountPer, billAmount){
+function calculatetotalTipAmount(amountPer, billAmount){
     let tipAmountPerPerson
     tipAmountPerPerson =  (amountPer/100) * billAmount;
-    console.log(tipAmountPerPerson)
+    return tipAmountPerPerson
 }
 
+function splitTips(){
+    let noOfPeople ;
+    noOfPeople = totalPeople.value;
+    console.log(noOfPeople);
+    tipPerPerson = tipAmountCalculated / noOfPeople;
+    console.log(tipPerPerson)
+}
 
 fivePer.addEventListener('click', () => tipPercentage(fivePer));
+totalPeople.addEventListener('onchange',splitTips);
